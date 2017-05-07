01d1ea23e5106c7c61a8b9e095edb64232204378
ah744 <ah744@cornell.edu>
2017-05-06 22:17:57 -0500

    added ising model implementation

---
+++ ising.py
@@ -0,0 +1,88 @@
+import math
+import numpy as np
+
+from projectq import MainEngine
+from projectq.backends import CircuitDrawer
+from projectq.ops import H, Z, X, Measure, All, R, CNOT, Rz
+from projectq.meta import Loop, Compute, Uncompute, Control
+
+def CZ(q1, q2, phi):
+#    Rz( 1 ) | q2
+    Rz( -2*phi ) | q2
+    CNOT | (q1, q2)
+#    Rz( 1 ) | q2
+    Rz( phi ) | q2
+    CNOT | (q1, q2)
+
+def ZcrossZ(q1, q2, phi):
+#    Rz(1) | q1
+#    Rz(1) | q2
+    Rz(phi) | q1
+    Rz(-1*phi) | q2
+    CZ(q1,q2,-2*phi)
+
+def Red(qubits, m, J, M):
+    for n in range(0,len(qubits)-1,2):
+        phi = J[n] * (2.0*m - 1)/M
+#        phi = 1
+        ZcrossZ(qubits[n], qubits[n+1], phi)
+
+def Black(qubits, m, J, M):
+    for n in range(1,len(qubits)-1,2):
+        phi = J[n] * (2.0*m - 1)/M
+#        phi = 1
+        ZcrossZ(qubits[n], qubits[n+1], phi)
+
+def Inter(qubits, m, Bx, Bz, T, M):
+    theta1 = (1.0 - (2.0*m - 1)/M) * -2 * Bx * T / M
+    for n in range(len(qubits)):
+        theta2 = (1.0 - (2.0*m-1)/M)* -2 * Bz[n] * T / M
+        H | qubits[n]
+        Rz(theta1) | qubits[n]
+#        Rz(1) | qubits[n]
+        H | qubits[n]
+#        Rz(1) | qubits[n]
+        Rz(theta2) | qubits[n]
+
+def run_ising(eng, N, Bx, Bz, J, M, T):
+    """
+    Runs Ising Model Hamiltonian simulation on n = 10 qubits 
+    Args:
+        eng (MainEngine): Main compiler engine to run Ising on.
+        N (int): Number of bits in the problem.
+        Bx (int): Bound on range of randomized rotations
+        M:  Number of Trotter steps to perform
+        T:  Total duration of adiabatic evolution
+    Returns:
+        solution (list<int>): Solution bit-string.
+    """
+    x = eng.allocate_qureg(N)
+
+    # start in uniform superposition
+    All(H) | x
+
+#    for m in range(M):
+#        Red(x, m, J, M)
+#        Black(x, m, J, M)
+#        Inter(x, m, Bx, Bz, T, M)
+
+    Measure | x 
+    eng.flush()
+
+      # return result
+    return [int(qubit) for qubit in x]
+
+if __name__ == "__main__":
+    drawing_engine = CircuitDrawer()
+    eng = MainEngine()  # use default compiler engine
+
+    Bx = 2
+    M = 25
+    T = 15
+    N = 5 
+    Bz = np.random.uniform(-Bx,Bx,N)
+    J = np.random.uniform(-Bx,Bx,N)
+
+    # run Ising Model with n = N qubits 
+    print(run_ising(eng, N, Bx, Bz, J, M, T))
+#    print(drawing_engine.get_latex())
